e834efcf738444c3f30d7eaf05b0ad2cda2b2aca
Brandon Smith <brundolf@Brandons-MBP.lan1>
2019-09-14 09:43:55 -0500

    Tidying up and set up global 404 page

--- src/index.js
+++ src/index.js
@@ -1,25 +1,31 @@
 
 
 // express
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
 const fs = require('fs').promises
+const path = require('path')
 
-const { ncpPromise } = require('./utils/misc')
+const ncp = require('ncp').ncp;
+const express = require('express')
+const bodyParser = require('body-parser')
+
 const blog = require('./render/blog.html');
 
+const app = express()
 app.use(express.static('dist', { extensions: [ 'html' ] }))
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use((req, res) => res.status(404).sendFile(path.resolve(__dirname, '../dist/404.html')))
 
 const PORT = process.env.PORT || 3000
+
+const ncpPromise = (from, to, options = {}) => new Promise(res => ncp(from, to, options, res)); 
+
 
 Promise.resolve()
     .then(() => fs.mkdir('./dist',      { recursive: true }))                      // make sure dirs exist
     .then(() => fs.mkdir('./dist/blog', { recursive: true }))
     .then(() => ncpPromise('./src/static', './dist'))                              // copy statics
     .then(() => Promise.all(                                                       // generate plain pages
-        [ '404', 'about', 'contact', 'index', 'resume' ].map(pageName =>
+        [ '404', 'about', 'contact', 'index' ].map(pageName =>
             require(`./render/${pageName}.html.js`)()
                 .then(html => fs.writeFile(`./dist/${pageName}.html`, html)))))
     .then(() =>                                                                    // generate blog pages
@@ -31,9 +37,6 @@ Promise.resolve()
                         .then(html => fs.writeFile(`./dist/blog/${post}.html`, html))))))
     .then(() =>                                                                    // start server
         app.listen(PORT, () => console.log(`Website listening on port ${PORT}!`)))
-
-
-
 
 
 
