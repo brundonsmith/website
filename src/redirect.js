2c9611dfaaba64c0f848fe14ad95fa6f14f6e3e1
Brandon Smith <mail@brandonsmith.ninja>
2021-04-09 18:40:50 -0500

    Fix redirect logic

--- src/redirect.js
+++ src/redirect.js
@@ -5,9 +5,9 @@ const redirect = (req, res, next) => {
     console.log(req.protocol, req.hostname)
     if (req.protocol !== 'https' || req.hostname !== DOMAIN) {
         res.redirect(301, BASE_URL + req.originalUrl);
+    } else {
+        next();
     }
-
-    next();
 }
 
 module.exports = redirect;
