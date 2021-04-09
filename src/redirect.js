70e5c88aa3ce5623bc2565a0edcb41ac7c26c88f
Brandon Smith <mail@brandonsmith.ninja>
2021-04-09 18:42:46 -0500

    Logging tweak

--- src/redirect.js
+++ src/redirect.js
@@ -2,8 +2,8 @@
 const { DOMAIN, BASE_URL } = require('./utils/constants')
 
 const redirect = (req, res, next) => {
-    console.log(req.protocol, req.hostname)
     if (req.protocol !== 'https' || req.hostname !== DOMAIN) {
+        console.log("REDIRECTING TO '" + BASE_URL + req.originalUrl + "'")
         res.redirect(301, BASE_URL + req.originalUrl);
     } else {
         next();
