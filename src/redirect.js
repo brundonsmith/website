5245cd425b6ae58bd27fccc8c3c7da1bc403e4bc
Brandon Smith <mail@brandonsmith.ninja>
2021-04-09 18:39:54 -0500

    Tweak redirect logic

--- src/redirect.js
+++ src/redirect.js
@@ -2,8 +2,9 @@
 const { DOMAIN, BASE_URL } = require('./utils/constants')
 
 const redirect = (req, res, next) => {
+    console.log(req.protocol, req.hostname)
     if (req.protocol !== 'https' || req.hostname !== DOMAIN) {
-        res.redirect(301, BASE_URL + req.path);
+        res.redirect(301, BASE_URL + req.originalUrl);
     }
 
     next();
