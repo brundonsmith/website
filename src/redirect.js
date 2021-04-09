96079db48870ca1e96c2bd43f8ce89e20227d551
Brandon Smith <mail@brandonsmith.ninja>
2021-04-09 18:48:22 -0500

    Add logging

--- src/redirect.js
+++ src/redirect.js
@@ -2,6 +2,10 @@
 const { DOMAIN, BASE_URL } = require('./utils/constants')
 
 const redirect = (req, res, next) => {
+    console.log(
+        `req.protocol !== 'https' || req.hostname !== DOMAIN | `,
+        req.protocol !== 'https' || req.hostname !== DOMAIN
+    )
     if (req.protocol !== 'https' || req.hostname !== DOMAIN) {
         console.log("REDIRECTING TO '" + BASE_URL + req.originalUrl + "'")
         return res.redirect(301, BASE_URL + req.originalUrl);
