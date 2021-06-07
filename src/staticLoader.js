3d88263efa78cea4daea26f6d614bdb9d12880a5
Brandon Smith <mail@brandonsmith.ninja>
2021-06-06 21:38:25 -0500

    Enable gzipping

--- src/staticLoader.js
+++ src/staticLoader.js
@@ -1,6 +1,9 @@
 const fs = require('fs').promises
 const path = require('path')
 
+const { gzip } = require('zlib');
+const gzipPromise = (buf) => new Promise((res, rej) => gzip(buf, (err, out) => err ? rej(err) : res(out)));
+
 // hand-wrote a replacement for express.static, so that files can be cached in 
 // memory instead of reloaded from disk
 
@@ -14,30 +17,43 @@ const CONTENT_TYPES = {
     'xml': 'application/rss+xml',
 }
 
-const readStaticFile = (filePath) => 
-    fs.readFile(path.resolve(__dirname, '../dist', filePath))
-        .then(contents => ({ 
-            contentType: CONTENT_TYPES[path.extname(filePath).substr(1)], 
-            contents 
-        }))
+const readStaticFile = async (filePath) => {
+    const contents = await fs.readFile(path.resolve(__dirname, '../dist', filePath));
+    const gzippedContents = await gzipPromise(contents);
+    console.log(gzippedContents)
+
+    const responseBuf = Buffer.alloc(gzippedContents.byteLength);
+    gzippedContents.copy(responseBuf);
+    return { 
+        contentType: CONTENT_TYPES[path.extname(filePath).substr(1)], 
+        contents: responseBuf,
+    }
+}
 
         
 const staticCache = { };
 
-const staticLoader = (req, res, next) => {
-    const filePath = (req.path.length < 2 ? '/index.html' : req.path.includes('.') ? req.path : req.path + '.html').substr(1);
+const staticLoader = async (req, res, next) => {
+    const filePath = (
+          req.path.length < 2 ? '/index.html' 
+        : req.path.includes('.') ? req.path 
+        : req.path + '.html'
+    ).substr(1);
 
-    const cached = staticCache[filePath];
+    if (staticCache[filePath] == null) {
+        try {
+            staticCache[filePath] = await readStaticFile(filePath);
+        } catch {
+            next(); // fallthrough to 404
+            return;
+        }
+    }
 
-    (cached ? Promise.resolve(cached) : readStaticFile(filePath).then(f => staticCache[filePath] = f))
-        .then(file => {
-            res.set('Content-Type', file.contentType);
+    const file = staticCache[filePath];
 
-            const responseBuf = Buffer.alloc(file.contents.byteLength);
-            file.contents.copy(responseBuf);
-            res.send(responseBuf);
-        })
-        .catch(() => next()) // fallthrough to 404
+    res.set('Content-Type', file.contentType);
+    res.set('Content-Encoding', 'gzip');
+    res.send(file.contents);
 }
 
 module.exports = staticLoader
