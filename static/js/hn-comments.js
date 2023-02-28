4a53a9bbfdfcbbd33ce40228e156b5af10f44921
Brandon Smith <mail@brandons.me>
2023-02-28 00:58:03 -0600

    Don't request HN comments unless on a relevant page

--- static/js/hn-comments.js
+++ static/js/hn-comments.js
@@ -1,18 +1,21 @@
 
 async function loadComments() {
     const container = document.querySelector('#hn-comments')
-    const res = await fetch(`/hn-comments/${encodeURIComponent(window.postName)}`)
-        .then(res => res.status === 200 ? res.json() : undefined)
 
-    if (container && res) {
-        container.innerHTML = `
-            <h4>
-                Comments from
-                <a href="https://news.ycombinator.com/item?id=${res.postId}" target="_blank">
-                    Hacker News</a>:
-            </h4>
-            ${res.html}
-        `
+    if (container) {
+        const res = await fetch(`/hn-comments/${encodeURIComponent(window.postName)}`)
+            .then(res => res.status === 200 ? res.json() : undefined)
+
+        if (res) {
+            container.innerHTML = `
+                <h4>
+                    Comments from
+                    <a href="https://news.ycombinator.com/item?id=${res.postId}" target="_blank">
+                        Hacker News</a>:
+                </h4>
+                ${res.html}
+            `
+        }
     }
 }
 
